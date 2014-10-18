5bbf826d22ebd14078cabe0f76db9f5dedd45805
Iskar Waluyo <iskarwaluyo@gmail.com>
2014-10-18 15:14:50 -0500

    Update complete.R

--- complete.R
+++ complete.R
@@ -1,14 +1,13 @@
-## RETURNS NULL FOR NOBS :-(
-
 complete <- function(directory, id = 1:322) 
 {
   setwd("~/R/R/specdata")
   for(i in id){
   filename <- sprintf("%03d", id)
   dataselect <- lapply(paste(directory,filename, ".csv",sep=""), read.csv, header=TRUE)
-  complete <- na.omit(dataselect)
-  nobs <- nrow(complete)
+  cleandata <- lapply (dataselect, na.omit, header=TRUE)
+  nobs <- lapply (cleandata, nrow)
   casematrix <- cbind(id, nobs)
   }
+
   return (casematrix)
 } 
