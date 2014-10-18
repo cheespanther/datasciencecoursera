653a95b3d15cac7e3ad1f8ec3bf5c6793b65b582
Iskar Waluyo <iskarwaluyo@gmail.com>
2014-10-18 11:42:55 -0500

    Update complete.R

--- complete.R
+++ complete.R
@@ -7,10 +7,8 @@ complete <- function(directory, id = 1:322)
   filename <- sprintf("%03d", id)
   dataselect <- lapply(paste(directory,filename, ".csv",sep=""), read.csv, header=TRUE)
   complete <- na.omit(dataselect)
-  nobs <- nrow(na.omit(dataselect))
+  nobs <- nrow(complete)
   casematrix <- cbind(id, nobs)
   }
-
-
   return (casematrix)
 } 
