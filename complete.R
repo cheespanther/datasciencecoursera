3e10e71748634b4a4420e54d291ce86da189b7c8
Iskar Waluyo <iskarwaluyo@gmail.com>
2014-10-18 10:35:48 -0500

    Create complete.R

---
+++ complete.R
@@ -0,0 +1,16 @@
+## ONLY WORKS FOR ONE ENTRY
+
+completecases <- function(directory, id = 1:322) 
+{
+  setwd("~/R/R/specdata")
+  for(i in id){
+  filename <- sprintf("%03d", id)
+  dataselect <- lapply(paste(directory,filename, ".csv",sep=""), read.csv, header=TRUE)
+  complete <- na.omit(dataselect)
+  nobs <- nrow(na.omit(dataselect))
+  casematrix <- cbind(id, nobs)
+  }
+
+
+  return (casematrix)
+} 
